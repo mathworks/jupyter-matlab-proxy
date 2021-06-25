9a5dd22c0d6cce461430a1c45cec44728f74836c
Prabhakar Kumar <prabhakk@mathworks.com>
2021-06-25 18:09:54 +0530

    Clear variables initialized in matlab_startup_file

--- jupyter_matlab_proxy/matlab/startup.m
+++ jupyter_matlab_proxy/matlab/startup.m
@@ -1,18 +1,23 @@
 % Copyright 2020-2021 The MathWorks, Inc.
 
 % Configure logged in user if possible
-first_name = getenv('MW_LOGIN_FIRST_NAME');
-last_name = getenv('MW_LOGIN_LAST_NAME');
-email_address = getenv('MW_LOGIN_EMAIL_ADDRESS');
-user_id = getenv('MW_LOGIN_USER_ID');
-profile_id = getenv('MW_LOGIN_PROFILE_ID');
-display_name = getenv('MW_LOGIN_DISPLAY_NAME');
-if (strlength(user_id) > 0)
+if ~isempty(getenv('MW_LOGIN_USER_ID'))
+    user_id = getenv('MW_LOGIN_USER_ID');
+    first_name = getenv('MW_LOGIN_FIRST_NAME');
+    last_name = getenv('MW_LOGIN_LAST_NAME');
+    email_address = getenv('MW_LOGIN_EMAIL_ADDRESS');
+    profile_id = getenv('MW_LOGIN_PROFILE_ID');
+    display_name = getenv('MW_LOGIN_DISPLAY_NAME');
+
     li = com.mathworks.matlab_login.MatlabLogin.isUserLoggedIn(2, 'DESKTOP');
     token = li.getToken();
     login_level = 2;
     remember_me = true;
-    com.mathworks.matlab_login.MatlabLogin.saveCacheLoginInfo(first_name, last_name, email_address, user_id, token, profile_id, login_level, remember_me, email_address, display_name);
+    com.mathworks.matlab_login.MatlabLogin.saveCacheLoginInfo(first_name, ...
+        last_name, email_address, user_id, token, profile_id, login_level, ...
+        remember_me, email_address, display_name);
+    % Clear all local variables from users workspace.
+    clear user_id first_name last_name email_address profile_id display_name token remember_me
 end
 
 if (strlength(getenv('BASE_URL')) > 0)
@@ -26,4 +31,4 @@ matlab_settings = settings;
 matlab_settings.matlab.addons.explorer.addSetting('isExplorerSupported');
 matlab_settings.matlab.addons.explorer.isExplorerSupported.PersonalValue = false;
 
-clear
+clear matlab_settings
