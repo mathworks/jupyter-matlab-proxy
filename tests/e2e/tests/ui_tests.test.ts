3cc2deb426b363dd4c59c0585a522ff5c4f818c2
Kumar Pallav <kpallav@mathworks.com>
2025-11-27 05:56:55 +0000

    Adds a step to build lezer-matlab module and improves end-to-end testing framework.

--- tests/e2e/tests/ui_tests.test.ts
+++ tests/e2e/tests/ui_tests.test.ts
@@ -1,25 +1,20 @@
-// Copyright 2023-2024 The MathWorks, Inc.
+// Copyright 2023-2025 The MathWorks, Inc.
 
 import { expect, test } from '@jupyterlab/galata';
-import { Locator } from '@playwright/test';
 
 test.describe('MATLAB File button', () => {
-    let MATLABFileButton: Locator;
-    test.beforeEach(async ({ page }) => {
-        await page.launcher.waitFor({ state: 'visible' });
-        MATLABFileButton = page.getByText('MATLAB File', { exact: true });
-    });
-
     test('is visible on Launcher', async ({ page }) => {
+        const MATLABFileButton = page.getByRole('button', { name: 'MATLAB File' });
         await expect(MATLABFileButton).toBeVisible();
     });
 
     test('takes you to .m file', async ({ page }) => {
+        const MATLABFileButton = page.getByRole('button', { name: 'MATLAB File' });
         await MATLABFileButton.click();
 
         // Expect a new untitled file ending in .m to be made, with optional
         // digits at the end of untitled, i.e. untitled1.m untitled1232.m)
-        await expect(page).toHaveURL(/.*untitled(\d+)?.m/);
+        await page.waitForURL(/.*untitled(\d+)?.m/);
 
         // MATLAB language mode selected:
         await expect(page.locator('#jp-main-statusbar')
